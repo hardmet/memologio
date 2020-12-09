b342b16180559103bbab67d992b5b6b09007828b
elly <borkafedor@mail.ru>
2020-12-09 20:52:48 +0300

    edit README.md, add testing instructions

---
+++ src/main/resources/db_initialization.sql
@@ -0,0 +1,10 @@
+create database memologio;
+commit;
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+create table post(
+    id uuid default uuid_generate_v4() not null primary key,
+    url       varchar(512)                    not null,
+    published timestamp                       not null,
+    likes     integer                         not null
+);
